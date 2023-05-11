d02f118e63f4de714625bc992741d58f3af623b6
ebergeng <59417456+ebergeng@users.noreply.github.com>
2023-05-11 08:00:22 +0200

    ds

--- src/js/components/eventlistners/buttonlistners/editProfileBtn.mjs
+++ src/js/components/eventlistners/buttonlistners/editProfileBtn.mjs
@@ -9,16 +9,12 @@ export function editButton() {
         if (displayEdit) {
             editBody.classList.remove("d-none")
             profileBody.classList.add("d-none")
-            btn.innerHTML = ""
-            btn.classList.add("btn-close", "me-3", "mt-2")
             displayEdit = false
             
         }else {
             editBody.classList.add("d-none")
             profileBody.classList.remove("d-none")
             displayEdit = true
-            btn.classList.remove("btn-close", "me-3", "mt-2")
-            btn.innerHTML = "Edit"
         }
     })
 }
